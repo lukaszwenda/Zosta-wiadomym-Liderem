8e2bba74c7f3415bd70855134334447b9f0e32d4
lukaszwenda <lukaszwenda@yahoo.co.uk>
2025-05-16 20:44:50 +0200

    Delete main (1).js

--- main (1).js
+++
@@ -1,9 +0,0 @@
-
-document.addEventListener("DOMContentLoaded", function () {
-  const app = document.getElementById("app");
-  if (app) {
-    app.innerHTML = "<p>✅ Quiz załadowany pomyślnie! Możesz rozpocząć.</p>";
-  } else {
-    document.body.innerHTML += "<p style='color: red;'>❌ Błąd: Nie znaleziono kontenera #app</p>";
-  }
-});
