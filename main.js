12e9675b2c89d2c1307e232d7d85c8da4f833372
lukaszwenda <lukaszwenda@yahoo.co.uk>
2025-05-16 20:34:47 +0200

    Delete main.js

--- main.js
+++
@@ -1,5 +0,0 @@
-
-document.addEventListener("DOMContentLoaded", () => {
-  const app = document.getElementById("app");
-  app.innerText = "Quiz został załadowany. Wersja testowa działa poprawnie!";
-});
