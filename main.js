51eb80620e68d7dd7d6fea4007db009e1bdef4e6
lukaszwenda <lukaszwenda@yahoo.co.uk>
2025-05-16 20:17:27 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,5 @@
+
+document.addEventListener("DOMContentLoaded", () => {
+  const app = document.getElementById("app");
+  app.innerText = "Quiz został załadowany. Wersja testowa działa poprawnie!";
+});
